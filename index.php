4432614a1a34e13e0622403ac1d656e57ce50f2f
Jeovani Marcos Morales Nieto <31924255+Creasick@users.noreply.github.com>
2018-03-01 01:28:59 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if ($method == "POST") {
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
+
+    $date_time = $json->result->parameters->date_time;
+    $address = $json->result->parameters->address;
+    $unit = $json->result->parameters->unit;
+	
+	$speech = "Entraste al webhook y el clima para $date_time en $address para $unit no lo se";
+
+	$response = new \stdClass   ();
+    $response-> speech = "";
+    $response-> displayText= "";
+    $response-> source ="webhook";
+    echo json_decode($response) 
+
+}
+else{
+    echo "Method no allowed";
+}
+?>
